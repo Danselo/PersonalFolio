ee4310a737a822c3a09624245d1249478427f0a1
Danselo <danselo185@gmail.com>
2024-03-11 22:17:19 -0500

    se agrego para verificar server

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -12,43 +12,43 @@ const Courses = () => {
         },
         {
             title: "Curso de Asincronismo con JavaScript",
-            icon: "src/assets/curso-asincronismo-js-removebg-preview.png",
+            icon: "/assets/curso-asincronismo-js-removebg-preview.png",
         },
         {
             title: "Curso de ECMAScript: Historia y Versiones de JavaScript",
-            icon: "src/assets/curso-ecmascript-removebg-preview.png",
+            icon: "/assets/curso-ecmascript-removebg-preview.png",
         },
         {
             title: "Curso de Frontend Developer",
-            icon: "src/assets/curso-frontend-removebg-preview.png",
+            icon: "/assets/curso-frontend-removebg-preview.png",
         },
         {
             title: "Curso de Introducción a la Terminal y Línea de Comandos",
-            icon: "src/assets/curso-introducción-linea-comandos-removebg-preview.png",
+            icon: "/assets/curso-introducción-linea-comandos-removebg-preview.png",
         },
         {
             title: "Curso de JavaScript Engine (V8) y el Navegador",
-            icon: "src/assets/curso-javascript-engine-removebg-preview.png",
+            icon: "/assets/curso-javascript-engine-removebg-preview.png",
         },
         {
             title: "Curso de React.js con Vite.js y TailwindCSS",
-            icon: "src/assets/curso-react-vite-tailwin-removebg-preview.png",
+            icon: "/assets/curso-react-vite-tailwin-removebg-preview.png",
         },
         {
             title: "Curso de React.js (Platzi)",
-            icon: "src/assets/curso-react-removebg-preview.png",
+            icon: "/assets/curso-react-removebg-preview.png",
         },
         {
             title: "Curso de Responsive Design: Maquetación Mobile First",
-            icon: "src/assets/curso-responsive-design-removebg-preview.png",
+            icon: "/assets/curso-responsive-design-removebg-preview.png",
         },
         {
             title: "Prueba Técnica: E-commerce Profesional con React.js",
-            icon: "src/assets/prueba-tecnica-ecommerce-react-removebg-preview.png",
+            icon: "/assets/prueba-tecnica-ecommerce-react-removebg-preview.png",
         },
         {
             title: "Curso Práctico de JavaScript",
-            icon: "src/assets/curso-practico-javascript.png",
+            icon: "/assets/curso-practico-javascript.png",
         },
 
     ];
